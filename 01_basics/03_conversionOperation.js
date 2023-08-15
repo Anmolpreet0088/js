9b815e1406a1fceb4d8c5935d1ba5ee10be473e7
Anmolpreet0088 <80095596+Anmolpreet0088@users.noreply.github.com>
2023-08-15 09:47:51 +0000

    Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,44 @@
+//let score = null;
+
+//let score = undefined;
+
+//let score = true;
+
+let score = "Anmol"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33"  =>  33
+// "33abc" =>  NaN
+// true => 1;  false => 0
+
+//let isLoggedIn = 1
+
+//let isLoggedIn = ""
+
+let isLoggedIn = "Anmol"
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false ; "Anmol" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
+
+
+
+
