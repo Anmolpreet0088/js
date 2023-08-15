cc923ff80453830dc8f52fcbc70beee2af8af3fe
Anmolpreet0088 <80095596+Anmolpreet0088@users.noreply.github.com>
2023-08-15 09:17:06 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict";  // treat all JS code as newer version
+
+// alert(3+3) //we are using node js , not browser
+
+console.log(3+3);
+
+console.log("Anmol");
+
+let name = "Anmol"
+let age = 18;
+let isLoggedIn = true;
+let state;
+
+//number => 2 to power 53
+//bigInt
+//string  => ""
+//boolean => true/false
+//null => stanalone value
+//undefined
+//symbol => unique
+//object =>
+
+
+console.log(typeof null)  // object
+
+
+console.log(typeof undefined)  //undefined
+
+
+
+
+
+
